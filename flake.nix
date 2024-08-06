b0ac873e15eca493204ec1a505f7911a706b36c2
rvn <rvn@rnet>
2024-08-06 20:07:14 +0900

    root flake attempted fix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   description = "Templates";
   outputs=
-  {  ... }:
-{
+  { self, ... }: {
+
   templates = {
+
   node = {
    path = ./nodejs;
    description = "NodeJS flake";
   };
+
   };
 };
 }
